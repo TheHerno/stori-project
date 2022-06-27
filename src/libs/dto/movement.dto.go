e659bcc2205712139b92a963f703b47f044939dd
Hernan <hernan@ohana.digital>
2022-06-27 01:00:30 -0300

    Fix dto groups

--- src/libs/dto/movement.dto.go
+++ src/libs/dto/movement.dto.go
@@ -8,6 +8,6 @@ import (
 MovementList is a DTO to list all Movements of a customer
 */
 type MovementList struct {
-	Customer  *entity.Customer  `json:"customer"`
-	Movements []entity.Movement `json:"movements"`
+	Customer  *entity.Customer  `json:"customer" groups:"client"`
+	Movements []entity.Movement `json:"movements" groups:"client"`
 }
